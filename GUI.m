509252d0bcab222185cab4b979ba29ee7cfd5687
Maycol <42551238+Sorbete-Sempai123@users.noreply.github.com>
2020-08-20 01:40:34 -0500

    Add files via upload

--- GUI.m
+++ GUI.m
@@ -7,7 +7,20 @@ graphics_toolkit qt
 h.ax = axes ("position", [0.05 0.42 0.5 0.5]);
 h.fcn = @(x) polyval([-0.1 0.5 3 0], x);
 
-funcAux = "@"
+% Metodo de Euler
+function [tt, yy] = Euler(f,x0,y0,h,xf)
+ k = ceil((xf - x0)/h);
+ tt = zeros(k,1);
+ yy = zeros(k,1);
+ tt(1) = x0;
+ yy(1) = y0;
+ for i = 2:k
+ tt(i) = tt(i-1) + h;
+ m = f(tt(i-1),yy(i-1));
+ yy(i) = yy(i-1) + h*m;
+ end
+end
+
 
 function update_plot (obj, init = false)
 
